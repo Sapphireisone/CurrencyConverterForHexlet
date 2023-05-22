32d966c99a7b8cfabb672ee7c84126c1e2bd261d
Sapphireisone <allmediasale@icloud.com>
2023-05-22 21:02:44 +0700

    added function finish

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import onlinerequests
-# 1. Берем данные котировок с сайта
+
 LIST = onlinerequests.response.json()
 CURRENCIES = LIST.get('data')
 
-# 2. Приветствие
 print('Добро пожаловать в конвертер!')
 print('''
 Наша программа поможет вам конвертировать валюту
@@ -12,45 +11,53 @@ print('''
 3. Выбор валюты для конвертации
 ''')
 
-# 3. Пишем список валют
-print('Вам предложены следующие валюты:')
-key_counter = 1
-for key in CURRENCIES:
-    print(f'{key_counter}. {key}')
-    key_counter += 1
-
-# 4. Вводим имеющуюся валюту
 while True:
-    user_currency = input('Введите имеющуюся валюту: ')
-    if user_currency in CURRENCIES.keys():
-        print('Отлично! Идем дальше...')
+    print('Вам предложены следующие валюты:')
+    key_counter = 1
+    for key in CURRENCIES:
+        print(f'{key_counter}. {key}')
+        key_counter += 1
+
+    while True:
+        user_currency = input('Введите имеющуюся валюту: ')
+        if user_currency in CURRENCIES.keys():
+            print('Отлично! Идем дальше...')
+            break
+        else:
+            print('Неверные данные...')
+
+    while True:
+        current_amount = input('Введите имеющуюся сумму: ')
+        try:
+            x = float(current_amount)
+            print('Отлично! Продолжаем...')
+            break
+        except:
+            print("Неверные данные...")
+
+    sum_amount = float(current_amount)
+
+    while True:
+        conversion_currency = input('Введите необходимую валюту: ')
+        if conversion_currency in CURRENCIES.keys():
+            break
+        else:
+            print('Неверные данные...')
+
+    converted_amount = sum_amount / CURRENCIES.get(user_currency) * CURRENCIES.get(conversion_currency)
+    print(f'Вы получите: {round(converted_amount, 2)} {conversion_currency}')
+
+    finish = input('Хотите повторить рассчеты? Y/N')
+    if finish == 'N':
         break
-    else:
-        print('Неверные данные...')
-
-# 5. Вводим сумму
-while True:
-    current_amount = input('Введите имеющуюся сумму: ')
-    try:
-        x = float(current_amount)
-        print('Отлично! Продолжаем...')
+    elif finish != 'Y':
+        i = 2
+        while finish != 'Y' and i > 0:
+            print('Неверные данные...')
+            finish = input('Хотите повторить рассчеты? Выберите: Y/N')
+            i -= 1
+        print('Слишком много неправильных попыток')
         break
-    except:
-        print("Неверные данные...")
-
-sum_amount = float(current_amount)
-
-# 6. Выбираем валюту конвертации
-while True:
-    conversion_currency = input('Введите необходимую валюту: ')
-    if conversion_currency in CURRENCIES.keys():
-        break
-    else:
-        print('Неверные данные...')
-
-# 7. Считаем и выдаем результат
-converted_amount = sum_amount / CURRENCIES.get(user_currency) * CURRENCIES.get(conversion_currency)
-print(f'Вы получите: {round(converted_amount, 2)} {conversion_currency}')
 
 
 
